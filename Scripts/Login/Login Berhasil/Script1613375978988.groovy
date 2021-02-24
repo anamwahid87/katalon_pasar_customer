8409576c744445c9c663e570e4c068e909371816
ddbqa <ddbqa@10.233.6.220>
2021-02-24 16:32:26 +0700

    testing lagi

--- Scripts/Login/Login Berhasil/Script1613375978988.groovy
+++ Scripts/Login/Login Berhasil/Script1613375978988.groovy
@@ -36,44 +36,6 @@ WebUI.navigateToUrl(GlobalVariable.url_prod)
 
 WebUI.waitForPageLoad(5)
 
-
-public class AllowGeoLocationPopup {
-	public static final String AUTOMATE_USERNAME = "YOUR_USERNAME";
-	public static final String AUTOMATE_KEY = "YOUR_ACCESS_KEY";
-	public static final String URL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
-  
-	public static void main(String[] args) throws Exception {
-	  DesiredCapabilities caps = new DesiredCapabilities();
-	  caps.setCapability("browser", "Chrome");
-	  caps.setCapability("browser_version", "75.0");
-	  caps.setCapability("os", "Windows");
-	  caps.setCapability("os_version", "10");
-  
-	  // INIT CHROME OPTIONS
-	  ChromeOptions options = new ChromeOptions();
-	  Map < String, Object > prefs = new HashMap < String, Object > ();
-	  Map < String, Object > profile = new HashMap < String, Object > ();
-	  Map < String, Object > contentSettings = new HashMap < String, Object > ();
-  
-	  // SET CHROME OPTIONS
-	  // 0 - Default, 1 - Allow, 2 - Block
-	  contentSettings.put("geolocation", 1);
-	  profile.put("managed_default_content_settings", contentSettings);
-	  prefs.put("profile", profile);
-	  options.setExperimentalOption("prefs", prefs);
-  
-	  // SET CAPABILITY
-	  caps.setCapability(ChromeOptions.CAPABILITY, options);
-	  WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-	  driver.get("https://the-internet.herokuapp.com/geolocation");
-	  driver.findElement(By.xpath("//*[@id='content']/div/button")).click();
-	  Thread.sleep(5000);
-	  driver.quit();
-	}
-  }
-  
-  
-
 WebUI.click(findTestObject('Object Repository/Page_Pasar.id/btn_ok'), FailureHandling.OPTIONAL)
 
 WebUI.acceptAlert(FailureHandling.OPTIONAL)
